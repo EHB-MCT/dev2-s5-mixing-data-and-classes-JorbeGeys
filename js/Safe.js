7d9a0a36c3c85b5b3b31de67c2a47bd376dab708
Jorbe Geys <jorbe.geys@student.ehb.be>
2023-03-22 16:11:00 +0100

    les 5 oef jokes

---
+++ js/Safe.js
@@ -0,0 +1,7 @@
+"use strict";
+
+export default class Safe {
+    constructor(safe){
+        this.safe = safe;
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,15 +1,33 @@
 "use strict";
 
+import Joke from "./Joke.js";
+
 const app = {
     items: [],
     init() {
+        this.getData();
     },
     getData() {
+        fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&amount=10&type=twopart')
+            .then(function (response){
+                return response.json();
+            })
+            .then(function (json){
+                json.jokes.forEach(function(jokeData){
+                    const joke = new Joke(jokeData.category , jokeData.setup , jokeData.delivery);
+                    app.items.push(joke);
+                    app.render(joke);
+                });
+            });
+            
     },
     onSearch() {
     },
     render(message) {
+        console.log(app.items);
+        document.querySelector('#list').insertAdjacentHTML('beforeend',message.htmlString);
     }
 
-}
-app.init()
+};
+
+app.init();
